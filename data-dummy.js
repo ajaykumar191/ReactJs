1247be53d4305f80c2e6e273586afca6ee84c5ea
ajay12aa <122091340+ajay12aa@users.noreply.github.com>
2025-05-19 12:32:52 +0530

    End of the Chatbot

---
+++ data-dummy.js
@@ -0,0 +1,25 @@
+const messages = [{
+    message: "Hello chatbot",
+    sender: "user",
+    image: "user.png",
+    id: 'id1'
+},
+{
+    message: "Hello! How can I help you?",
+    sender: "robot",
+    image: "robot.png",
+    id: 'id2'
+
+},
+{
+    message: "what is the date today",
+    sender: "user",
+    image: "user.png",
+    id: 'id3'
+},
+{
+    message: "Today is May 17",
+    sender: "robot",
+    image: "robot.png",
+    id: 'id4'
+}]
