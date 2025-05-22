eac7e5acabb327457086ba713828e250024e1052
ajay12aa <122091340+ajay12aa@users.noreply.github.com>
2025-05-22 16:29:49 +0530

    Added 404 Error file

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
+import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <>
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<OrdersPage />} />
         <Route path="tracking" element={<TrackingPage />} />
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
 
     </>
